45b02f163e69a458a7de5c9635b84203be0112cb
autaut03 <autaut03@gmail.com>
2019-02-18 20:37:58 +0200

    Fix incorrect registry name for block holder

--- src/main/java/net/alexwells/roomery/mechanic/roomholder/Constants.kt
+++ src/main/java/net/alexwells/roomery/mechanic/roomholder/Constants.kt
@@ -2,4 +2,4 @@ package net.alexwells.roomery.mechanic.roomholder
 
 import net.alexwells.roomery.Roomery
 
-val ROOM_HOLDER_RESOURCE = Roomery.createResource("something")
+val ROOM_HOLDER_RESOURCE = Roomery.createResource("room_holder")
